9f45533c1ea8d889ecd4bdbe3855d7a7b61052de
Veso <Ambroza8888@gmail.com>
2019-10-10 18:43:04 -0700

    Progress

---
+++ Login_Registration/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Login_Registration.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Login_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext dbContext;
+        public HomeController(MyContext context)
+        {
+            dbContext = context;
+        }
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+        [HttpPost("/Register")]
+        public IActionResult Register(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                if(dbContext.Users.Any(u => u.Email == user.Email))
+                {
+                    ModelState.AddModelError("Email","Email already in use!");
+                    return View("Index");
+                }
+                else if(dbContext.Users.Any(u => u.FirstName == user.FirstName))
+                {
+                    ModelState.AddModelError("FirstName","First Name is already in use!");
+                    return View("Index");
+                }
+                else if(dbContext.Users.Any(u => u.LastName == user.LastName))
+                {
+                    ModelState.AddModelError("LastName","Last Name is already in use!");
+                    return View("Index");
+                }
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                user.Password = Hasher.HashPassword(user,user.Password);
+                dbContext.Add(user);
+                dbContext.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View("Index");
+        }
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // ---------------------------------------------------------------------
+        // ERRORRR
+        // ---------------------------------------------------------------------
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Login_Registration/Models/User.cs
@@ -0,0 +1,44 @@
+    using System.ComponentModel.DataAnnotations;
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+    namespace Login_Registration.Models
+    {
+        public class User
+        {
+
+            [Key]
+            public int UserId { get; set; }
+
+            [Required]
+            [MinLength(2, ErrorMessage="First Name must be more than 2")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [MinLength(2, ErrorMessage="Last Name must be more than 2")]
+            
+            public string LastName { get; set; }
+
+            
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [MinLength(4, ErrorMessage="Password must be 4 characters or longer!")]
+            public string Password { get; set; }
+            // -----------------------------------------------------------------
+            // date
+            // -----------------------------------------------------------------
+            public DateTime CreatedAt {get;set;} = DateTime.Now;
+            public DateTime UpdatedAt {get;set;} = DateTime.Now;
+
+            [NotMapped]
+            [Compare("Password")]
+            [DataType(DataType.Password)]
+            public string Confirm {get;set;}
+        }
+    }
