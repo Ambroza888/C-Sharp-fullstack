6a60ae649a532dd3ba56b8dc8baab58cb7f72c8e
Veso <Ambroza8888@gmail.com>
2019-10-14 19:12:09 -0700

    LogIn setted up

--- WeddingPlanner/Controllers/HomeController.cs
+++ WeddingPlanner/Controllers/HomeController.cs
@@ -5,20 +5,137 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WeddingPlanner.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 
 namespace WeddingPlanner.Controllers
 {
     public class HomeController : Controller
     {
+        private MyContext dbContext;
+        public HomeController(MyContext context)
+        {
+            dbContext = context;
+        }
+        // ---------------------------------------------------------------------
+        // Index
+        // ---------------------------------------------------------------------
+        [HttpGet("")]
         public IActionResult Index()
         {
-            return View();
+            return View("index");
+        }
+        // ---------------------------------------------------------------------
+        // Registrating to db
+        // ---------------------------------------------------------------------
+        [HttpPost("/Register")]
+        public IActionResult Register(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                if(dbContext.Users.Any(u => u.Email == user.Email))
+                {
+                    ModelState.AddModelError("Email","Email already in use!");
+                    return View("Index");
+                }
+                else if(dbContext.Users.Any(u => u.FirstName == user.FirstName))
+                {
+                    ModelState.AddModelError("FirstName","First Name is already in use!");
+                    return View("Index");
+                }
+                else if(dbContext.Users.Any(u => u.LastName == user.LastName))
+                {
+                    ModelState.AddModelError("LastName","Last Name is already in use!");
+                    return View("Index");
+                }
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                user.Password = Hasher.HashPassword(user,user.Password);
+                dbContext.Add(user);
+                dbContext.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View("Index");
+        }
+        // ---------------------------------------------------------------------
+        // Login Page
+        // ---------------------------------------------------------------------
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+        // ---------------------------------------------------------------------
+        // Log in TO success or BELT EXAM would be this page !
+        // ---------------------------------------------------------------------
+        [HttpPost("logToSuccess")]
+        public IActionResult LoginSuccess(LoginUser userSubmision)
+        {
+            if(ModelState.IsValid)
+            {
+                var userInDb = dbContext.Users.FirstOrDefault(u => u.Email == userSubmision.Email);
+                if(userInDb == null)
+                {
+                    ModelState.AddModelError("Email","Invalid Email/Password");
+                    return View("Login");
+                }
+                var hasher = new PasswordHasher<LoginUser>();
+                var result = hasher.VerifyHashedPassword(userSubmision,userInDb.Password,userSubmision.Password);
+                if(result == 0)
+                {
+                    ModelState.AddModelError("Password","Invalid Email/Password");
+                    return View("Login");
+                }
+                //--------------------- Creating session after all those checks -------------------
+                int user_id = userInDb.UserId;
+                HttpContext.Session.SetInt32("user_id",user_id);
+                return RedirectToAction("Success");
+            }
+            return View("Login");
+        }
+        // ---------------------------------------------------------------------
+        // Success PAGE potential belt-exam page
+        // ---------------------------------------------------------------------
+
+        [HttpGet("Success")]
+        public IActionResult Success()
+        {
+            if(HttpContext.Session.GetInt32("user_id") == null)
+            {
+                return RedirectToAction("Login");
+            }
+                return View("Success");
         }
 
-        public IActionResult Privacy()
+        [HttpGet("clear")]
+        public IActionResult clearSession()
         {
-            return View();
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // ---------------------------------------------------------------------
+        // ERRORRR
+        // ---------------------------------------------------------------------
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
